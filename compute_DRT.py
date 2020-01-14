cdcaf682e6b100c5abe4ca0b4a92ea858d517495
ciuccislab <suntan1991@gmail.com>
2020-01-14 20:19:16 +0800

    update

--- compute_DRT.py
+++ compute_DRT.py
@@ -47,28 +47,5 @@ def A_im(freq):
                 
     return out_A_im
 
-def L(freq):
-    
-    tau = 1./freq
-    N_freqs = freq.size
-    
-    out_L = np.zeros((N_freqs-2, N_freqs))
-    
-    for p in range(0, N_freqs-2):
-
-        delta_loc = log(tau[p+1]/tau[p])
-        
-        if p==0 or p == N_freqs-3:
-            out_L[p,p] = 2./(delta_loc**2)
-            out_L[p,p+1] = -4./(delta_loc**2)
-            out_L[p,p+2] = 2./(delta_loc**2)
-        else:
-            out_L[p,p] = 1./(delta_loc**2)
-            out_L[p,p+1] = -2./(delta_loc**2)
-            out_L[p,p+2] = 1./(delta_loc**2)
-
-    return out_L
-
-
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
